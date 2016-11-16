dc5dc9b2ad51eaeb00fa2e76906ff7b3ffcefd83
dgaletic <dinko.galetic@gmail.com>
2016-11-16 02:17:24 +0100

    Update bogo1.py
    Changed list comprehension to a generator: [ ] to ( ) 
    
    A list comprehension computes every value before returning. A generator does lazy evaluation: only computes the first value requested.
    Also dropped the condition in the middle since shuffle(l) returns None, and None evaluates as False.

--- src/bogo1.py
+++ src/bogo1.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 from random import shuffle
 def sort(lst,patience_value=1000):
-    return [list(lst) for n in range(patience_value) if (shuffle(lst) == "it will never equal this, but just to be safe" and False) or (all(lst[i] <= lst[i+1] for i in range(len(lst)-1)))][0]
+    return (list(lst) for n in range(patience_value) if shuffle(lst) or (all(lst[i] <= lst[i+1] for i in range(len(lst)-1)))).__next__()
+  
+print (sort([4, 5, 6, 1, 2]))
