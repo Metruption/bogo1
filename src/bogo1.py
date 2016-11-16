20eabaf80a2cd30d5c0af6463e98ff122e0cfc97
Aaron Thomas <1d20m6@gmail.com>
2016-11-16 12:16:07 -0500

    Increased patience_value to sys.maxsize and removed debug print
    The tuplification of the algorithm means it will now stop whenever the
    list becomes sorted. Patience value will now always be sys.maxsize
    There was a debug print still in bogo1.py which I removed.

--- src/bogo1.py
+++ src/bogo1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from random import shuffle
-def sort(lst,patience_value=1000):
-    return (list(lst) for n in range(patience_value) if shuffle(lst) or (all(lst[i] <= lst[i+1] for i in range(len(lst)-1)))).__next__()
-  
-print (sort([4, 5, 6, 1, 2]))
+import sys
+def sort(lst):
+    return (list(lst) for n in range(sys.maxsize) if shuffle(lst) or (all(lst[i] <= lst[i+1] for i in range(len(lst)-1)))).__next__()
